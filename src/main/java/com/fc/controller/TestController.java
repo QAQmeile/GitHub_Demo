2ccf1562b76fc73716fe5470e4267f1e850019bf
Buffer <3324047771@qq.com>
2021-05-08 17:10:35 +0800

    第三次提交，增加了一个方法

--- src/main/java/com/fc/controller/TestController.java
+++ src/main/java/com/fc/controller/TestController.java
@@ -3,5 +3,10 @@ package com.fc.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println(1);
+
+    }
+//    假设这个方法是项目中的查询全部的方法
+    public void test(){
+
     }
 }
